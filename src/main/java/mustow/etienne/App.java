44801900a4d620cf5361c31722f66560b53a6343
aclaudel <arnaud.claudel@codurance.com>
2019-11-27 15:09:12 +0000

    App added

---
+++ src/main/java/mustow/etienne/App.java
@@ -0,0 +1,29 @@
+package mustow.etienne;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.Spark;
+
+import static spark.Spark.get;
+
+public class App {
+
+    public static final int DEFAULT_PORT = 8080;
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        int port = getPort();
+        LOGGER.info("The {} will be used", port);
+        Spark.port(port);
+        get("/", (req, res) -> "Hello World");
+    }
+
+    private static int getPort() {
+        String portString = System.getenv("PORT");
+        if(portString == null) {
+            return DEFAULT_PORT;
+        }
+        return Integer.parseInt(portString);
+    }
+
+}
