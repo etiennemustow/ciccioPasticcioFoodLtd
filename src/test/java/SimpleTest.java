6a592c7193eb1a2aa108643fc635a506cb702813
Etienne Mustow <etienne.o.mustow@gmail.com>
2019-11-27 11:51:06 +0000

    circle ci added

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+    @Test
+    public void should_return_true_for_true() {
+        assertEquals(true, true);
+    }
+}
