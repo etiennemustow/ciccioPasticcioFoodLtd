f3f0bcc4c1a887315d6e6af7f8ace2cda2867f2a
aclaudel <arnaud.claudel@codurance.com>
2019-11-28 12:07:14 +0000

    sql files

---
+++ src/main/resources/create_ciccio.sql
@@ -0,0 +1,1 @@
+CREATE TABLE ciccio_user (id uuid, name varchar(20) NOT NULL);
---
+++ src/main/resources/sample_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO ciccio_user VALUES ('b32e8598-6ec3-43d7-a724-02af2505a8f1', 'Bob');
+INSERT INTO ciccio_user VALUES ('4daee82b-7ef7-4107-8450-d9313fd08c8e', 'Alice');
